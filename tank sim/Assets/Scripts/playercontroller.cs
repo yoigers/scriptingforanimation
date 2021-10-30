cdcee16cc18962fe31c75cd5fcf6887b27efb344
Afton <10954265@uvu.edu>
2021-10-30 16:01:22 -0600

    added tank forward movement

---
+++ tank sim/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed = 10.5f;
+    public float turnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+    }
+}
