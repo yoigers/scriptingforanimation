c1588a759bdf443b1db3a28ab080a0a5795388a6
Afton <10954265@uvu.edu>
2021-10-30 21:55:16 -0600

    added keyboard controls and camera follo

--- tank sim/Assets/Scripts/playercontroller.cs
+++ tank sim/Assets/Scripts/playercontroller.cs
@@ -7,6 +7,9 @@ public class playercontroller : MonoBehaviour
     public float speed = 10.5f;
     public float turnSpeed;
 
+    public float hInput;
+    public float vInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,11 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        // Moves the tank left and right
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+        // Moves the tank forward and back
+       transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput); 
     }
 }
