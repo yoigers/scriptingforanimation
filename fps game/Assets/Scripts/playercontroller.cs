b9be6b8fe4576da75bae5433344f32190c315a5c
Afton <10954265@uvu.edu>
2021-12-04 21:40:27 -0700

    fps player movement and jumping motions

--- fps game/Assets/Scripts/playercontroller.cs
+++ fps game/Assets/Scripts/playercontroller.cs
@@ -6,12 +6,12 @@ public class playercontroller : MonoBehaviour
 {
     // Movement speed in unitspersecond, force applied upwards
     public float moveSpeed;
-    public float jumpforce;
+    public float jumpForce;
 
     // Mouse-look sensitivity, limited up-down vision, current x rotation on camera
     public float lookSensitivity;
-    public float maxLookX = -41f;
-    public float minLookX = 34f;
+    public float maxLookX;
+    public float minLookX;
     private float rotX;
 
     private Camera camera;
@@ -21,7 +21,7 @@ public class playercontroller : MonoBehaviour
     void Start()
     {
         // Get components
-        camera = camera.main;
+        camera = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -29,21 +29,48 @@ public class playercontroller : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
     }
 
 
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+    }
+
+    // Move around, move where camera goes
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        // rb.velocity = new Vector3(x, rb.velocity.y, z); --this is old code
+        Vector3 dir = transform.right * x + transform.forward * z;
+        rb.velocity = dir;
     }
 
-
+    // Use mouse to look around
     void CamLook()
     {
         float y = Input.GetAxis("Mouse X") * lookSensitivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    // Player can jump
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, 1.1f))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
